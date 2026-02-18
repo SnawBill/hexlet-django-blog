f603011f387d84b6bcd67ef99f4621a9174f9f41
SnawBill <vladmalov201@gmail.com>
2026-02-18 14:58:53 +1000

    add index.htmland about.html

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for hexlet_django_blog project.
 
@@ -16,7 +17,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
 
 urlpatterns = [
+    path('', views.index),
+    path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
